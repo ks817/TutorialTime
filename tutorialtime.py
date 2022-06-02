54c14721ad5722b8407968295d5bfb1d0b75d49c
Kusum Sharma <ks17@umbc.edu>
2022-06-02 14:37:01 -0400

    Add Python implimentation of tutorialtime

---
+++ tutorialtime.py
@@ -0,0 +1,53 @@
+import math
+
+SECTOHOUR = 0.0002777778
+MINTOHOUR = 0.0166666667
+
+def askInput():
+
+    timearray = []
+
+    #input function always returns string so convert to int
+    hour = int(input("\nPlease input the Hours for the tutorial or enter 0 to quit.\n"))
+
+    if hour == 0:
+        return 0
+    else:
+        minute = float(input("Minutes: "))
+        sec = float(input("Seconds: "))
+        getHour = converttohour(hour,minute,sec)
+        dt = doubleTime(getHour)
+        timearray.append(dt)
+    
+    return timearray
+
+def converttohour(hour,minute,sec):
+    regTime = hour + minute*MINTOHOUR + sec*SECTOHOUR
+    return regTime
+
+def doubleTime(regTime):
+    x2 = input("Do you want to double the time? (y/n) ")
+    if x2 == "y" or x2 == "Y":
+        x2time = 2 * regTime
+        return x2time
+    else:
+        return regTime
+
+def main():
+    #calculate tutorial hours
+
+    index = 0
+    total = 0
+
+    hours = askInput() 
+    
+    while hours != 0 and len(hours) > 0:
+        total += hours[index]
+        hours = askInput()
+    
+    print("This tutorial will take you", total, "hours to finish\n")
+    print("goodbye")
+
+    return 0
+
+main()
